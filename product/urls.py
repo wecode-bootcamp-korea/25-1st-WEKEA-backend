54094d4c428b78e775d8ddd491383b9676c4c956
jh <bndgc@naver.com>
2021-10-14 00:51:33 +0900

    [Add] RandomProductView 추가

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from product.views import CategoryListView, ProductListView, ProductView
+from product.views import CategoryListView, ProductListView, ProductView, RandomProductView
 
 urlpatterns = [
     path('/categories', CategoryListView.as_view()),
     path('/subcategories', ProductListView.as_view()),
     path('/<int:product_id>', ProductView.as_view()),
+    path('/random', RandomProductView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -87,3 +87,46 @@ class ProductView(View):
         }]
 
         return JsonResponse({'product' : result}, status = 200)
+
+class RandomProductView(View):
+    def get(self, request):
+        products = Product.objects.order_by('?')[:9]
+
+        result = [{
+            "id"            : product.id,
+            "foreign_name"  : product.foreign_name,
+            "korea_name"    : product.korea_name,
+            "price"         : product.price,
+            "information"   : product.information,
+            "description"   : product.description,
+            "main_cateogry" : {
+                "id"   : product.sub_category.main_category.id,
+                "name" : product.sub_category.main_category.name,
+            },
+            "sub_cateogry"  : {
+                "id"          : product.sub_category.id,
+                "name"        : product.sub_category.name,
+                "description" : product.sub_category.description,
+            },
+            "reviews" : [{
+                    "id"               : review.id,
+                    "review"           : review.text,
+                    "installation"     : review.installation,
+                    "cost_performance" : review.cost_performance,
+                    "quality"          : review.quality,
+                    "appearance"       : review.appearance,
+                    "functionality"    : review.functionality,
+                } for review in product.reviews.all()],
+            "images" : [{
+                    "id"            : image.id,
+                    "product_image" : image.product_image,
+                } for image in product.product_images.all()],
+            "sizes" : [{
+                    "id"     : size.id,
+                    "width"  : size.width,
+                    "length" : size.length,
+                    "height" : size.height,
+                } for size in product.product_sizes.all()]
+        } for product in products]
+
+        return JsonResponse({'product' : result}, status = 200)
