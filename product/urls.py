8537b4a45bb4f9fad12960cc82521e4b0086a1d8
jh <bndgc@naver.com>
2021-10-14 12:43:37 +0900

    PR 수정사항 - subcategoryid -> sub_category_id - RamdomProductView 삭제

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from product.views import CategoryListView, ProductListView, ProductView, RandomProductView
+from product.views import CategoryListView, ProductListView, ProductView 
 
 urlpatterns = [
     path('/categories', CategoryListView.as_view()),
     path('/subcategories', ProductListView.as_view()),
     path('/<int:product_id>', ProductView.as_view()),
-    path('/random', RandomProductView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -3,7 +3,7 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from product.models import MainCategory, SubCategory, Product
+from product.models import MainCategory, Product
 
 class CategoryListView(View):
     def get(self, request):
@@ -22,8 +22,7 @@ class CategoryListView(View):
 
 class ProductListView(View): 
     def get(self, request):
-        sub_category_id = int(request.GET.get('subcategoryid', None))
-        print(sub_category_id)
+        sub_category_id = int(request.GET.get('sub_category_id', None))
         products = Product.objects.filter(sub_category_id = sub_category_id)
 
         result = [{
@@ -66,67 +65,24 @@ class ProductView(View):
                 "description" : product.sub_category.description,
             },
             "reviews" : [{
-                    "id"               : review.id,
-                    "review"           : review.text,
-                    "installation"     : review.installation,
-                    "cost_performance" : review.cost_performance,
-                    "quality"          : review.quality,
-                    "appearance"       : review.appearance,
-                    "functionality"    : review.functionality,
+                "id"               : review.id,
+                "review"           : review.text,
+                "installation"     : review.installation,
+                "cost_performance" : review.cost_performance,
+                "quality"          : review.quality,
+                "appearance"       : review.appearance,
+                "functionality"    : review.functionality,
                 } for review in product.reviews.all()],
             "images" : [{
-                    "id"            : image.id,
-                    "product_image" : image.product_image,
+                "id"            : image.id,
+                "product_image" : image.product_image,
                 } for image in product.product_images.all()],
             "sizes" : [{
-                    "id"     : size.id,
-                    "width"  : size.width,
-                    "length" : size.length,
-                    "height" : size.height,
+                "id"     : size.id,
+                "width"  : size.width,
+                "length" : size.length,
+                "height" : size.height,
                 } for size in product.product_sizes.all()]
         }
-
-        return JsonResponse({'product' : result}, status = 200)
-
-class RandomProductView(View):
-    def get(self, request):
-        products = Product.objects.order_by('?')[:9]
-
-        result = [{
-            "id"            : product.id,
-            "foreign_name"  : product.foreign_name,
-            "korea_name"    : product.korea_name,
-            "price"         : product.price,
-            "information"   : product.information,
-            "description"   : product.description,
-            "main_cateogry" : {
-                "id"   : product.sub_category.main_category.id,
-                "name" : product.sub_category.main_category.name,
-            },
-            "sub_cateogry"  : {
-                "id"          : product.sub_category.id,
-                "name"        : product.sub_category.name,
-                "description" : product.sub_category.description,
-            },
-            "reviews" : [{
-                    "id"               : review.id,
-                    "review"           : review.text,
-                    "installation"     : review.installation,
-                    "cost_performance" : review.cost_performance,
-                    "quality"          : review.quality,
-                    "appearance"       : review.appearance,
-                    "functionality"    : review.functionality,
-                } for review in product.reviews.all()],
-            "images" : [{
-                    "id"            : image.id,
-                    "product_image" : image.product_image,
-                } for image in product.product_images.all()],
-            "sizes" : [{
-                    "id"     : size.id,
-                    "width"  : size.width,
-                    "length" : size.length,
-                    "height" : size.height,
-                } for size in product.product_sizes.all()]
-        } for product in products]
 
         return JsonResponse({'product' : result}, status = 200)
